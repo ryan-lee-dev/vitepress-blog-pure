5289d7a8ab3e217b8b2b3f599c80241b9d3ffe6b
ryan <ryan.dev.vip@gmail.com>
2025-11-22 23:59:50 +0800

    fix static assets

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,8 +7,8 @@ const pageSize = 10
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-    title: 'Vitepress blog',
-    base: '/',
+    title: 'Ryan\s Blog',
+    base: '/blog',
     cacheDir: './node_modules/vitepress_cache',
     description: 'vitepress,blog,blog-theme',
     ignoreDeadLinks: true,
