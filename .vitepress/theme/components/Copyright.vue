489a615a7bbf39e26b6af51be3d8f902f4e6fdbe
ryan <ryan.dev.vip@gmail.com>
2025-11-23 11:11:03 +0800

    modify copy right

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="site-footer">
-        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
-        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.6.4</a>
+        MIT Licensed | Copyright © 2025 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
+        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress</a>
         Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
     </div>
 </template>
