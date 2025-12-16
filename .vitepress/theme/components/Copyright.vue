c12cb634e7bba8d50b4601d4f3494fa4410f9c3c
ryan <ryan.dev.vip@gmail.com>
2025-12-16 23:31:55 +0800

    11

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -4,7 +4,6 @@
         Copyright © 2025 <a class="vitepress" :href="website">{{ webTitle }}</a> | <script defer src="https://vercount.one/js"></script>Viewed: <span id="vercount_value_site_uv"></span> <br/>
         Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress</a> And 
         Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
-        
     <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9af526794ac6469ca82aa59c961b0c0b"}'></script><!-- End Cloudflare Web Analytics -->
     </div>
 </template>
