62f28683025e71d66eaf5d21d9574796898bcf1a
ryan <ryan.dev.vip@gmail.com>
2025-12-16 23:02:11 +0800

    update foot

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,7 +1,7 @@
 <template>
     
     <div class="site-footer">
-        Copyright © 2025 <a class="vitepress" :href="website">{{ webTitle }}</a> | <script defer src="https://vercount.one/js"></script>Viewed: <span id="vercount_value_site_uv"></span> <br />
+        Copyright © 2025 <a class="vitepress" :href="website">{{ webTitle }}</a> | <script defer src="https://vercount.one/js"></script>Viewed: <span id="vercount_value_site_uv"></span> <br/>
         Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress</a>
         Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
     <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9af526794ac6469ca82aa59c961b0c0b"}'></script><!-- End Cloudflare Web Analytics -->
