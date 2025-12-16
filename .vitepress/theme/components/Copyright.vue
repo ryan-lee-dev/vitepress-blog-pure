e6a217de9a176f3c09203f1a1420d07195d7e654
ryan <ryan.dev.vip@gmail.com>
2025-12-16 23:50:42 +0800

    fix foot

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,10 +1,10 @@
 <template>
     
     <div class="site-footer">
+        <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9af526794ac6469ca82aa59c961b0c0b"}'></script><!-- End Cloudflare Web Analytics -->
+        Powered by <a class="vitepress" target="_blank" :href="vitepress">VitePress</a> |
+        Theme by <a class="vitepress" target="_blank" :href="theme999">Vitepress-blog</a><br/>
         Copyright © 2025 <a class="vitepress" :href="website">{{ webTitle }}</a> | <script defer src="https://vercount.one/js"></script>Viewed: <span id="vercount_value_site_uv"></span> <br/>
-        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress</a> And 
-        Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
-    <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9af526794ac6469ca82aa59c961b0c0b"}'></script><!-- End Cloudflare Web Analytics -->
     </div>
 </template>
 <script lang="ts" setup>
@@ -13,6 +13,8 @@ import { useData } from 'vitepress'
 const { site, theme } = useData()
 const website = theme.value.website
 const webTitle = site.value.title
+const vitepress = "https://vitepress.vuejs.org/"
+const theme999 = "https://github.com/airene/vitepress-blog-pure/"
 </script>
 
 <style>
